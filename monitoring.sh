290e3b0f9b1407f0770c1b2870b45930d2f98a1c
echavez- <echavez-@student.42.fr>
2023-01-31 12:27:19 +0100

    [+]monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,75 +1,3 @@
-# born2beroot
-### Operating system
-Debian 11.6
-
-## Requirements
-- [X] No graphical interface
-
-- [X] 2 encrypted partitions using LVM
-
-- [X] SSH service only running on port 4242
-
-- [X] Impossible to connect using SSH as root
-
-- [X] Configure operating system with the UFW
-
-### hostname
-- [X] Login ending with 42
-
-### Users
-- [X] Root user
-
-- [X] A user with the login as username, it has to belong to the user42 and sudo groups
-
-### pwd security
-|Requirements |Description  |
-|--- | --- |
-| **Expiration**|Every 30 days|
-| **Min number of days allowed before modification** |2|
-| **Warning message**|7 days before expiration|
-| **Length**|At least 10 characters|
-| **Composition**|An uppercase letter, a lowercase letter and a number|
-| **Forbidden**|More than 3 consecutive identical characters, include the name of the user|
-| **New password**|Must have at least 7 characters that aren't a part of the former password|
-
-### Configuration for sudo group
-- [X] Authentication of sudo caused by incorrect password is limited to 3 attempts.
-
-- [X] Display message if there is an error caused by an incorrect password.
-
-- [X] Archive every action of sudo (inputs and outputs). The log file has to be saved in: /var/log/sudo/
-
-- [X] TTY mode has to be enabled
-
-- [X] Paths that can be used by sudo must be restricted.
-
-### monitoring.sh
-
-Developed in bash, will display some information on all terminals every 10 min. No error must be visible.
-
-|Information for the script|
-| --- |
-|The architecture of your operating system and its kernel version|
-|Number of physical processors|
-|Number of virtual processors|
-|Current available RAM on your server and its utilization rate as a percentage|
-|Current available memory on your server and its utilization rate as a percentage|
-|Current utilization rate of your processors as a percentage|
-|Date and time of the last reboot|
-|Whether LVM is active or not|
-|Number of active connections|
-|Number of users using the server|
-| IPv4 address of your server and its MAC address|
-| Number of commands executed with the sudo program.|
-
-### Steps
-* Download and check debian distro:
-```bash
-echo '224cd...e6f51  debian-11.6.0-amd64-netinst.iso' | sha512sum --check
-debian-11.6.0-amd64-netinst.iso: OK
-```
-### monitoring script
-```bash
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
@@ -78,7 +6,7 @@ debian-11.6.0-amd64-netinst.iso: OK
 #    By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/31 12:22:02 by echavez-          #+#    #+#              #
-#     Updated: 2023/01/31 12:26:44 by echavez-         ###   ########.fr       #
+#    Updated: 2023/01/31 12:22:12 by echavez-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -140,4 +68,3 @@ wall "    #Architecture: $architecture
     #User log: $active_users
     #Network: $ip_mac
     #Sudo: $sudo_commands cmd"
-```
